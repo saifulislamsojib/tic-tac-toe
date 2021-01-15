44087ba06eea87fa2ea6a5bf6ea7662d4d2ebeb1
saifulislamsojib <saifulislam.sojib56@gmail.com>
2021-01-15 18:15:13 +0600

    g

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 let trueFalse = true;
+const a = [];
+const b = [];
+const all = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 document.querySelector('.boxes').addEventListener("click", (e) => {
+    const currentClick = Number(e.target.getAttribute("data-count"));
     game(e);
     e.target.style.cursor = 'not-allowed';
 })
-// document.querySelector('.boxes').addEventListener("mouseover", (e) => {
-//     game(e);
-//     e.target.style.cursor = 'not-allowed';
-// })
-
 function game(e) {
     if (e.target.innerHTML === '') {
         if (trueFalse === true) {
-            e.target.innerHTML = "<div class='circle'><div></div></div>";
+            console.log(e.target)
+            e.target.innerHTML += `<div class='circle'>
+                                    </div>`;
             trueFalse = false;
         } else {
-            e.target.innerHTML += '<div class="cross"><div class="cross-1"><div></div></div><div class="cross-2"><div></div></div></div>';
+            e.target.innerHTML += `<div class="cross">
+                                        <div class="cross-1">
+                                        </div>
+                                        <div class="cross-2">
+                                        </div>
+                                    </div>`;
             trueFalse = true;
         }
     }
