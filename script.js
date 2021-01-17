89077b352da2952664d9d2a18b267186c7aa7d6a
saifulislamsojib <saifulislam.sojib56@gmail.com>
2021-01-17 20:12:16 +0600

    sojib

--- script.js
+++ script.js
@@ -1,27 +1,62 @@
-let trueFalse = true;
-const a = [];
-const b = [];
-const all = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-document.querySelector('.boxes').addEventListener("click", (e) => {
-    const currentClick = Number(e.target.getAttribute("data-count"));
-    game(e);
-    e.target.style.cursor = 'not-allowed';
+let isTrue = true;
+const xPlayer = document.getElementById('x-player');
+const oPlayer = document.getElementById('o-player');
+const reset = document.getElementById('reset');
+players(xPlayer, false, oPlayer);
+players(oPlayer, true, xPlayer);
+function players(player, trueFalse, another) {
+    player.addEventListener('click', () => {
+        isTrue = trueFalse;
+        player.setAttribute('disabled', '');
+        player.classList.add("disabled");
+        another.classList.remove("disabled");
+        another.removeAttribute('disabled');
+    });
+}
+const win = {
+    w1: [0, 1, 2],
+    w2: [3, 4, 5],
+    w3: [6, 7, 8],
+    w4: [0, 3, 6],
+    w5: [1, 4, 7],
+    w6: [2, 5, 8],
+    w7: [0, 4, 8],
+    w8: [2, 4, 6]
+};
+const boxes = document.getElementsByClassName('box');
+[...boxes].map((box, i) => {
+    box.addEventListener('click', () => {
+        game(box);
+        box.style.cursor = 'not-allowed';
+    })
+    reset.addEventListener('click', () => {
+        box.innerHTML = '';
+        oPlayer.setAttribute('disabled', '');
+        oPlayer.classList.add("disabled");
+        xPlayer.classList.remove("disabled");
+        xPlayer.removeAttribute('disabled');
+        isTrue = true;
+    })
 })
-function game(e) {
-    if (e.target.innerHTML === '') {
-        if (trueFalse === true) {
-            console.log(e.target)
-            e.target.innerHTML += `<div class='circle'>
+function game(box) {
+    if (!box.innerHTML) {
+        if (isTrue) {
+            box.innerHTML += `<div class='circle'>
                                     </div>`;
-            trueFalse = false;
-        } else {
-            e.target.innerHTML += `<div class="cross">
-                                        <div class="cross-1">
-                                        </div>
-                                        <div class="cross-2">
-                                        </div>
-                                    </div>`;
-            trueFalse = true;
+            isTrue = false;
+            xPlayer.setAttribute('disabled', '');
+            xPlayer.classList.add("disabled");
+        }
+        else {
+            box.innerHTML += `<div class="cross">
+                                    <div class="cross-1">
+                                    </div>
+                                    <div class="cross-2">
+                                    </div>
+                                </div>`;
+            isTrue = true;
+            oPlayer.setAttribute('disabled', '');
+            oPlayer.classList.add("disabled");
         }
     }
 }
